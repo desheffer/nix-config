24646a29fcb7218393dc6cc98b7c66be5554ac21
Doug Sheffer <desheffer@gmail.com>
2022-06-11 02:56:06 -0400

    Add more GUI packages

--- home/modules/gui.nix
+++ home/modules/gui.nix
@@ -1,13 +1,25 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
-{
+let
+  cfg = config.homeConfig.gui;
+
+in {
   options.homeConfig.gui = {
     enable = mkOption {
       type = types.bool;
       description = "Whether to enable most GUI applications.";
       default = config.homeConfig.gnome.enable;
     };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gimp
+      qalculate-gtk
+      vlc
+      xsel
+    ];
   };
 }
