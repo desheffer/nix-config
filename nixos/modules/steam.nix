b10671d4aa156419a50798a90791e45b18c0b730
Doug Sheffer <desheffer@gmail.com>
2022-11-19 01:07:30 -0500

    Enable openFirewall options for Steam

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -18,6 +18,9 @@ in
   config = mkIf cfg.enable {
     programs.steam = {
       enable = true;
+
+      dedicatedServer.openFirewall = true;
+      remotePlay.openFirewall = true;
     };
 
     environment.systemPackages = with pkgs; [
