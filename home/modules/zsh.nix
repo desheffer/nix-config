a7d7d2b436e3ce5ce808d6a4b6b68e5e8943b69a
Doug Sheffer <desheffer@gmail.com>
2023-09-26 00:00:31 -0400

    Use pkgs.zsh-nix-shell

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -110,13 +110,8 @@ in
       plugins = [
         {
           name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.5.0";
-            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-          };
+          file = "share/zsh-nix-shell/nix-shell.plugin.zsh";
+          src = pkgs.zsh-nix-shell;
         }
       ];
     };
