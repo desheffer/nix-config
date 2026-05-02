a0bca8c24e915b8762ac49ea31ab7d1ab6259a23
Doug Sheffer <desheffer@gmail.com>
2026-05-01 22:05:35 -0400

    Add Copy Fail mitigation

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -26,6 +26,10 @@ in
           consoleMode = "0";
         };
       };
+
+      # BUG: CVE-2026-31431 ("Copy Fail"). Remove once all hosts are on a
+      # kernel containing a664bf3d603d (>= 6.12.85, 6.18.22, 6.19.12, or 7.0).
+      extraModprobeConfig = "install algif_aead /bin/false";
     };
   };
 }
