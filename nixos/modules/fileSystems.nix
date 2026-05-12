3d97b32b62bbc5415ddcfa26a71c62a2b2ec30fd
Doug Sheffer <desheffer@gmail.com>
2026-05-12 11:33:13 -0400

    Tighten /boot permissions

--- nixos/modules/fileSystems.nix
+++ nixos/modules/fileSystems.nix
@@ -70,6 +70,10 @@ in
       "/boot" = {
         device = "/dev/disk/by-label/BOOT";
         fsType = "vfat";
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
       };
     };
 
